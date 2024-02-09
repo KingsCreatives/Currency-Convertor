10ac25497851913b9d52952daebf905711799a11
Kwame Cody <kwamecody@gmail.com>
2024-02-09 04:52:04 +0000

    replaced fetch with axios

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((data) => setData(data[currency]))
 
-        console.log(data)
+        const fetchData = async () => {
+            try{
+                const response = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+                const data = response.data[currency]
+
+                setData(data)
+            }catch(err){
+                console.log(err)
+            }
+        }
+
+        fetchData()        
     }, [currency])
 }
 
