fb6ebb3f108e067dde0d73b8cf8482ee74dd40e9
Kwame Cody <kwamecody@gmail.com>
2024-02-10 20:46:38 +0000

    deleted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import useCurrencyInfo  from '../src/hooks/useCurrencyInfo';
 
 
-function App() {
-  const [amount , setAmount] = useState(0)
-  const [from , setFrom] = useState("ghs")
-  const [to, setTo] = useState("usd")
-  const [conversionResult, setConversionResult] = useState(0)
-
-  const currencyData = useCurrencyInfo(from)
-  const currencyOptions = Object.keys(currencyData)
-
-  const currencyConversion = setConversionResult(amount * currencyOptions[to])
-
-  const swap = () => {
-    setFrom(setTo)
-    setTo(setFrom)
-    setConversionResult(amount)
-    setAmount(conversionResult)
-  }
-
-
-
+function App(){
+  
   return (
-    <div
-    className='flex justify-center items-center mx-auto my-11 py-4  bg-gray-700 text-gray-500'
-    >
-       
-    </div>
+    <h1 className='bg-red-300'>Restarting project</h1>
   )
 }
 
