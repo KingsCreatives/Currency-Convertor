a1c23ed319f593982978824ebfe07da782256c2a
Kwame Cody <kwamecody@gmail.com>
2024-02-12 21:13:18 +0000

    input box component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import InputBox from './components/InputBox'
+
 
 
 function App(){
   
   return (
-    <h1 className='bg-red-300'>Restarting project</h1>
+      <div>
+          <InputBox/>
+      </div>
   )
 }
 
