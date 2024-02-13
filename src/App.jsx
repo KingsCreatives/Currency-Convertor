9daa19baddd7978af87a99d51d5142dbabb96496
Kwame Cody <kwamecody@gmail.com>
2024-02-13 17:16:00 +0000

    base style for app

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,36 @@ import InputBox from './components/InputBox'
 function App(){
   
   return (
-      <div>
-          <InputBox/>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-gray-950'
+    >
+       <div className='w-full'>
+        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+          }}>
+            <div className='w-full mb-1'>
+              <InputBox
+              label="from"
+              />
+            </div>
+            <div className='relative w-full h-0.5'>
+              <button
+              className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+              >Swap</button>
+            </div>
+            <div className='w-full mb-1'>
+              <InputBox
+              label="to"
+              />
+            </div>
+            <button
+            type='submit'
+            className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+            >Convert</button>
+          </form>
+        </div>
+      </div>
+
       </div>
   )
 }
